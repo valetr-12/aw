3bd78961a169b6d10e2dc93017e21856e9b0c46a
valetr-12 <vtamayor@unbosque.edu.co>
2025-04-30 14:44:16 -0500

    creacion proyecto

---
+++ advancewarsbk/src/main/java/co/edu/unbosque/advancewarsbk/AdvancewarsbkApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.advancewarsbk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancewarsbkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancewarsbkApplication.class, args);
+	}
+
+}
---
+++ advancewarsbk/src/test/java/co/edu/unbosque/advancewarsbk/AdvancewarsbkApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.advancewarsbk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancewarsbkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
