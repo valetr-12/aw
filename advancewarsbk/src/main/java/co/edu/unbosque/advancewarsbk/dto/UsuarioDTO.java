4d4b0414ab0e0f027385ef6736d9e075f5187973
Maryory007 <mgonzalezren@unbosque.edu.co>
2025-04-30 16:04:00 -0500

    DTO usuario

--- advancewarsbk/src/main/java/co/edu/unbosque/advancewarsbk/dto/UsuarioDTO.java
+++ advancewarsbk/src/main/java/co/edu/unbosque/advancewarsbk/dto/UsuarioDTO.java
@@ -2,4 +2,46 @@ package co.edu.unbosque.advancewarsbk.dto;
 
 public class UsuarioDTO {
 
+	private Integer id;
+	private String usuario;
+	private String contraseña;
+
+	public UsuarioDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public UsuarioDTO(String usuario, String contraseña) {
+		this.usuario = usuario;
+		this.contraseña = contraseña;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", usuario=" + usuario + ", contraseña=" + contraseña + "]";
+	}
+
 }
