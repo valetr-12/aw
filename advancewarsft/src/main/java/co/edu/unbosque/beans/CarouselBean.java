26ab412acb0a01f664f82a7027d164fdfc9ea1c3
valetr-12 <vtamayor@unbosque.edu.co>
2025-04-30 17:41:05 -0500

    carusel

---
+++ advancewarsft/src/main/java/co/edu/unbosque/beans/CarouselBean.java
@@ -0,0 +1,44 @@
+package co.edu.unbosque.beans;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.bean.ManagedBean;
+import jakarta.faces.bean.ViewScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@ManagedBean
+@ViewScoped
+public class CarouselBean implements Serializable {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private List<Item> items;
+
+    @PostConstruct
+    public void init() {
+        items = new ArrayList<>();
+        items.add(new Item("Images/ima1.png"));
+        items.add(new Item("Images/ima2.png"));
+        items.add(new Item("Images/ima3.png"));
+        items.add(new Item("Images/ima1.png"));
+        // Puedes añadir más imágenes si quieres
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public static class Item {
+        private String imagePath;
+
+        public Item(String imagePath) {
+            this.imagePath = imagePath;
+        }
+
+        public String getImagePath() {
+            return imagePath;
+        }
+    }
+}
